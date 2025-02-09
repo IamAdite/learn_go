47cf70876bd684a26bee94151246491486fb0807
Pavel Ogoreltsev <paul.ogorelcev@gmail.com>
2025-02-09 14:58:58 +0300

    added some err and health handlers

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"log"
 	"net/http"
 	"github.com/joho/godotenv"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 )
 
 func main() {
@@ -20,6 +20,21 @@ func main() {
 	
 	router := chi.NewRouter()
 
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Links"},
+		AllowCredentials: false,
+		MaxAge: 		  300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+
+	router.Mount("/v1", v1Router)
+
 	srv := &http.Server{
 		Handler: router,
 		Addr: ":" + portString,
@@ -30,6 +45,5 @@ func main() {
 	err := srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
-	}
-
+	}	
 }
